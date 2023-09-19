94f3cac9d60b23566f865def6ff76324f58500a3
Eduardo Zambaldi <eduardo.silva3@ab-inbev.com>
2023-09-19 23:14:31 +0200

    First commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Pet_Play"
+include(":androidApp")
+include(":shared")
---
+++ shared/src/androidUnitTest/kotlin/br/com/zambaldi/petplaykmm/androidTest.kt
@@ -0,0 +1,12 @@
+package br.com.zambaldi.petplaykmm
+
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+class AndroidGreetingTest {
+
+    @Test
+    fun testExample() {
+        assertTrue("Check Android is mentioned", Greeting().greet().contains("Android"))
+    }
+}
